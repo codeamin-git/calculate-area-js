fe08acd58e4b2095736fbeb6c6e3f4166211b7bb
codeamin11 <legacyofalamin@gmail.com>
2024-02-14 01:26:57 +0600

    pentagon card added included js

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,26 @@
 function calculateRectangleArea(){
-    console.log('rect btn clicked')
+    // get width of the rectangle
+    const rectWidth = document.getElementById('rect-width');
+    const rectWidthInput = rectWidth.value;
+    const width = parseFloat(rectWidthInput);
+    console.log(width);
+    
+    // get length of the rectangle
+    const rectLength = document.getElementById('rect-length');
+    const rectLengthInput = rectLength.value;
+    const length = parseFloat(rectLengthInput);
+    console.log(length);
+
+    // count the area
+    const area = length * width;
+    console.log(area);
+    
+    // set the calculated area to the website using innerText
+    const rectArea = document.getElementById('rect-area');
+    if(typeof area !== 'number' || isNaN(area)){
+        rectArea.innerText = 'invalid';
+    } else{
+        rectArea.innerText = area;
+    }
+ 
 }
